5e73057cc970b911c97ded98ec6f61ffcb834a83
Amal-Az <amalazour430@gmail.com>
2024-11-19 14:14:08 +0100

    premier commit

---
+++ env.js
@@ -0,0 +1,147 @@
+/* var firstName= "amal";
+var age = 23;
+let isStudent= false;
+const country= "maroc";
+console.log(firstName);
+console.log(age);
+console.log(isStudent);
+console.log(country); 
+
+let x=10;
+const y= "hello";
+var z = 10,z = 12;
+
+var a= 42;
+var b= "hello";
+console.log(typeof(a));
+console.log(typeof(b));
+var c= true;
+console.log(typeof(c));
+a= "mami";
+console.log(typeof(a));
+var x1;
+console.log("le type de x1 est "+ typeof(x1));
+let x2= null;
+console.log("le type de x2 est "+ typeof(x2));
+console.log("le type de x est "+ x2);
+
+var a=10
+var b="10"
+console.log(a===b)
+console.log(a==b)
+
+var x3= 15
+var y3= 5
+console.log(x3+y3)
+console.log(x3-y3)
+console.log(x3*y3)
+console.log(x3/y3)
+console.log(x3%y3)
+
+var greeting= "bonjour";
+var greetingUpper = (greeting +" tout le monde").toUpperCase();
+console.log(greetingUpper);
+
+var counter= 5
+console.log(++counter)
+console.log(--counter)
+
+var isAdmin= false
+var isLoggedIn= true
+var canAccess= true :
+    if isAdmin== true && isLoggedIn== true;
+    else; */
+  
+//var age;
+//console.log(age)
+  //if age:  
+    // console.log("Variable non définie");
+
+// let A = 8; 
+// let number= (A%2 === 0)? A +" est un nombre pair" : A + " est un nombre impair";
+// console.log(number);
+
+// let age = 18
+// if (age>=18){
+//   console.log(age +" est majeure")
+// }
+// else {console.log(age +" est mineur")}
+
+// var jourJ=6
+// if (jourJ === 1) {
+//   console.log("C'est le lundi !");
+// }
+// else if (jourJ === 2) {
+//   console.log("C'est le mardi !");
+// }
+// else if (jourJ === 3) {
+//   console.log("C'est le mercredi !");
+// }
+// else if (jourJ === 4) {
+//   console.log("C'est le jeudi !");
+// }
+// else if (jourJ === 5) {
+//   console.log("C'est le vendredi !");
+// }
+// else if (jourJ === 6) {
+//   console.log("C'est le samedi !");
+// }
+// else if (jourJ === 7) {
+//   console.log("C'est le dimanche !");
+// }
+
+// var number1 = 0;
+
+// switch (true) {
+//     case (number1 > 0):
+//         console.log(number1 + " est un nombre positif");
+//         break;
+//     case (number1 < 0):
+//         console.log(number1 + " est un nombre négatif");
+//         break;
+//     default:
+//         console.log(number1 +"Le nombre est nul");
+// }
+
+// let montant = 120;
+// if (montant > 100) {
+//     let reduction = montant * 0.1
+
+// ex 1
+// for(let i=1 ;i<11 ;i++){
+//     console.log("le nombre est: " +i)
+// }
+// Ex 2
+  // for (let i = 1; i <= 20; i++) {
+  //   if (i % 2 === 0) {
+  //       console.log(i);
+  //   }
+// }
+// Ex 3
+// for (let i=1 ; i <= 5; i++){
+//    console.log("le carrée est : " +i*i);
+// }
+// ex 4
+// for(let i=10 ;i>=1 ;i--){
+//         console.log("le nombre est: " +i);
+//     }
+// ex 5
+// for (let i = 1; i <= 9; i += 2) {
+//     console.log("nombre impaire : " +i);
+// }
+// ex 6  
+function hello(names,age){ 
+console.log("hello"+ names+ " j'ai "+ age);
+
+}
+hello(' amal', 23);
+
+var time= 34
+function addition(a, b) { 
+
+  return a + b; }
+  console.log(addition(4, 7))
+
+// addition (4, 7) 
+var x= addition(3,4)
+console.log(x+ " est la somme des nombres donnés ")  
