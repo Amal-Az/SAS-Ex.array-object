74659361e172ae9f5564f512a6e70b3ba47f3855
Amal-Az <amalazour430@gmail.com>
2024-11-19 16:00:47 +0100

    ex-array

---
+++ ex-array.js
@@ -0,0 +1,36 @@
+//Ex facile 1
+let tableau= [1,2,3,4,5]
+console.log(tableau)
+// 2
+let fruits = ["apple", "banana", "cherry"]
+console.log(fruits [1])
+// 3
+let numbers= [10, 20, 30, 40]
+numbers[2]=(50)
+console.log(numbers)
+// 4
+let sweet = ["mango", "peach", "pear"]
+sweet.push("orange")
+console.log(sweet)
+// 5
+let animal = ["dog", "cat", "bird"]
+animal.pop();
+console.log(animal)
+// 6
+let human= [prenom="Amal",age= 23]
+console.log(human)
+// 7
+let personne = [nom="John",age=25]
+console.log(personne)
+// 8
+let book={title:"JavaScript 101",author:"John Doe"}
+book.author="Jane Doe"
+console.log(book)
+// 9
+let car = {make:"Toyota",model:"Corolla"}
+car.year="2020"
+console.log(car)
+// 10
+let person= {nom:"Manal",age:25}
+let verif= "age" in person
+console.log(verif)
